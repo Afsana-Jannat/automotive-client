52c1c6d685ad11c46b722b8b5b7c0634dd4bfd32
afsana-jannat <afsanajannat205@gmail.com>
2023-10-20 17:30:05 +0600

    all okay

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
        <Navbar></Navbar>
        <Banner></Banner>
       <h1 className='text-6xl text-center mt-6
-       text-purple-400'>Automotive: {automotives.length}
+       text-red-800'>Automotive: {automotives.length}
        </h1> 
        <div className='grid md:grid-cols-2 lg:grid-cols-3
        gap-2 lg:ml-6 mt-10 mb-6'>
@@ -27,6 +27,16 @@ function App() {
           </AutomotiveCard>)
         }
        </div>
+       <div>
+          <h2 className="text-3xl font-semibold 
+          text-center mt-6 mb-6
+       text-red-800">
+            All the ways to find a better
+            car Repair Shop</h2>
+            <p className='text-center mb-8 text-red-900'>
+              New Atlas keeps you up to date on new cars, concepts, EVs and the latest automotive news including test drives,
+               reviews and coverage of all the major auto shows ...</p>
+        </div>
        <Footer></Footer>
 
       
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
           dropdown-end">
             <label tabIndex={0} className="btn btn-ghost
              btn-circle avatar">
-              <div className="w-10 rounded-full">
+              <div className="w-15 rounded-full">
                 <img src={user?.photoURL} />
               </div>
             </label>
